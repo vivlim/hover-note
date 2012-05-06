7999a9712ccd15be9c891e184cb0b7a932a8081b
unknown <mikel@amnesia-III.(none)>
2012-05-05 18:50:09 -0700

    can enter and exit focus with the overlay, and can close the overlay by pressing back while focused.

--- OverlayTest/src/com/mjlim/overlaytest/OverlayTest.java
+++ OverlayTest/src/com/mjlim/overlaytest/OverlayTest.java
@@ -4,16 +4,21 @@ import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnKeyListener;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.KeyEvent;
 
 
 public class OverlayTest extends Service {
 	
 	EditText tView;
+	OverlayView oView;
 	
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -24,19 +29,39 @@ public class OverlayTest extends Service {
 	@Override
 	public void onCreate(){
 		super.onCreate();
+		
+		final WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+		
 		Toast.makeText(getBaseContext(), "onCreate", Toast.LENGTH_LONG).show();
 		tView = new EditText(this);
-		tView.setHeight(40);
 		tView.setText("HELLO");
-		WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-				WindowManager.LayoutParams.TYPE_PHONE,
-				WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL);
-		params.gravity = Gravity.RIGHT | Gravity.TOP;
-		params.setTitle("HELLO!");
-		params.height = 60;
-		WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
-		wm.addView(tView,params);
 		
+		oView = new OverlayView(this, wm);
+//		WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+//				WindowManager.LayoutParams.TYPE_PHONE,
+//				WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL);
+//		params.gravity = Gravity.RIGHT | Gravity.TOP;
+//		params.setTitle("HELLO!");
+//		params.height = 60;
+		
+		
+//        tView.setOnKeyListener(new OnKeyListener(){
+//            public boolean onKey(View v, int keyCode, KeyEvent event) 
+//            {
+////            	wm.removeView(tView);
+//        		WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+//        				WindowManager.LayoutParams.TYPE_PHONE,
+//        				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+//        		params.gravity = Gravity.RIGHT | Gravity.TOP;
+//        		params.setTitle("HELLO!");
+//        		params.height = 60;
+//            	wm.updateViewLayout(tView, params);
+//            	Toast.makeText(getBaseContext(), "a keypress!", Toast.LENGTH_LONG).show();
+//                return false;
+//            }
+//        });
+        
+//		wm.addView(tView,params);
 	}
 	
 	@Override
--- OverlayTest/src/com/mjlim/overlaytest/OverlayView.java
+++ OverlayTest/src/com/mjlim/overlaytest/OverlayView.java
@@ -1,8 +1,115 @@
 package com.mjlim.overlaytest;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View.OnKeyListener;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 
-//
-//public class OverlayView extends ViewGroup {
-//
-//}
+
+public class OverlayView extends LinearLayout implements OnKeyListener, OnClickListener, OnTouchListener{
+	private TextView tv;
+	private EditText ed;
+	private boolean focused = true;
+	private WindowManager.LayoutParams winparams;
+	
+	private WindowManager wm;
+	
+	private Context context;
+	
+	public OverlayView(Context context, WindowManager wm){
+		super(context);
+		
+		this.context = context;
+		
+		this.wm = wm;
+		
+		winparams = new WindowManager.LayoutParams(
+						WindowManager.LayoutParams.TYPE_PHONE |
+						WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+						WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH);
+		
+		winparams.gravity = Gravity.RIGHT | Gravity.TOP;
+		winparams.setTitle("HELLO!");
+		winparams.height = 60;
+		this.setBackgroundColor(Color.BLUE);
+		
+		tv = new TextView(context);
+		tv.setText("Text");
+		
+		ed = new EditText(context);
+		ed.setText("default");
+		
+		this.addView(tv);
+		this.addView(ed);
+		
+		this.setOnTouchListener(this);
+		this.setOnClickListener(this);
+		this.setOnKeyListener(this);
+		ed.setOnKeyListener(this);
+		wm.addView(this, winparams);
+		
+	}
+	
+	public void focus(){		
+		this.focused = true;
+		this.setBackgroundColor(Color.BLUE); // visual cue
+		winparams.flags |= WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL; // set this flag on
+		winparams.flags |= WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH; // this too, gets unset when FLAG_NOT_TOUCH_MODAL is turned off.
+		winparams.flags &= ~WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE; // set this flag off
+    	wm.updateViewLayout(this, winparams);
+    	this.postInvalidate();//redraw
+    	tv.invalidate();
+
+	}
+	
+	public void unfocus(){	
+		this.focused = false;
+		this.setBackgroundColor(Color.BLACK); // visual cue
+		winparams.flags &= ~WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL; // set this flag off
+		winparams.flags |= WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE; // set this flag on
+    	wm.updateViewLayout(this, winparams);
+    	this.invalidate();//redraw
+    	tv.invalidate();
+	}
+	public WindowManager.LayoutParams getWindowParams(){
+		return winparams;
+	}
+	
+	public boolean onKey(View v, int keyCode, KeyEvent event) 
+    {
+//		tv.setText("key");
+
+//		this.unfocus();
+		if(keyCode == KeyEvent.KEYCODE_BACK){
+	        context.stopService(new Intent(context, OverlayTest.class));
+		}
+        return false;
+    }
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		this.focus();
+	}
+
+	public boolean onTouch(View v, MotionEvent me) {
+		// TODO Auto-generated method stub
+		if((me.getActionMasked() & MotionEvent.ACTION_OUTSIDE) == MotionEvent.ACTION_OUTSIDE){
+			this.unfocus();
+		}
+		else
+			this.focus();
+		return true;
+	}
+}
